b99e5025885ffc6badc4f84470587964cbddcca1
Florian Loitsch <florian@loitsch.com>
2026-05-02 16:23:21 +0200

    Replace eslint-plugin-import with eslint-plugin-import-x
    eslint-plugin-import 2.32.0 only supports eslint up to v9, blocking the
    upgrade to eslint v10. eslint-plugin-import-x is the actively maintained
    fork that supports eslint v10.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,10 @@
 // See: https://eslint.org/docs/latest/use/configure/configuration-files
 
-import { fixupPluginRules } from '@eslint/compat'
 import { FlatCompat } from '@eslint/eslintrc'
 import js from '@eslint/js'
 import typescriptEslint from '@typescript-eslint/eslint-plugin'
 import tsParser from '@typescript-eslint/parser'
-import _import from 'eslint-plugin-import'
+import importX from 'eslint-plugin-import-x'
 import jest from 'eslint-plugin-jest'
 import prettier from 'eslint-plugin-prettier'
 import globals from 'globals'
@@ -33,7 +32,7 @@ export default [
   ),
   {
     plugins: {
-      import: fixupPluginRules(_import),
+      'import-x': importX,
       jest,
       prettier,
       '@typescript-eslint': typescriptEslint
@@ -58,7 +57,7 @@ export default [
     },
 
     settings: {
-      'import/resolver': {
+      'import-x/resolver': {
         typescript: {
           alwaysTryTypes: true,
           project: 'tsconfig.eslint.json'
@@ -71,7 +70,7 @@ export default [
       'eslint-comments/no-use': 'off',
       'eslint-comments/no-unused-disable': 'off',
       'i18n-text/no-en': 'off',
-      'import/no-namespace': 'off',
+      'import-x/no-namespace': 'off',
       'no-console': 'off',
       'no-shadow': 'off',
       'no-unused-vars': 'off',
